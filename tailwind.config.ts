2b5590978f195e54cc4d157c1e558df132638212
SoolaimanG <111349654+SoolaimanG@users.noreply.github.com>
2023-09-13 04:37:06 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
  "./Pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./messages/**/*.{js,ts,jsx,tsx,mdx}", 
   ],
